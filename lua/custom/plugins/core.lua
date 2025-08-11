e1522cc7937301a9f4b6451e6112ab5d35133665
Anyang Ateny <anyangateny@outlook.com>
2025-08-11 13:24:04 +0930

    feat: added initial .dotfiles

---
+++ lua/custom/plugins/core.lua
@@ -0,0 +1,4 @@
+return {
+  -- Core plugins
+  'tpope/vim-sleuth',
+} 
---
+++ lua/custom/plugins/indent-blankline.lua
@@ -0,0 +1,32 @@
+-- Indent-blankline - Add indentation guides even on blank lines
+-- This plugin adds vertical lines to show indentation levels
+return {
+  'lukas-reineke/indent-blankline.nvim',
+  -- Enable `lukas-reineke/indent-blankline.nvim`
+  -- See `:help ibl`
+  main = 'ibl',
+  opts = {
+    indent = {
+      char = "│",
+      tab_char = "│",
+    },
+    scope = { 
+      enabled = false 
+    },
+    exclude = {
+      filetypes = {
+        "help",
+        "alpha",
+        "dashboard",
+        "neo-tree",
+        "Trouble",
+        "trouble",
+        "lazy",
+        "mason",
+        "notify",
+        "toggleterm",
+        "lazyterm",
+      },
+    },
+  },
+} 
