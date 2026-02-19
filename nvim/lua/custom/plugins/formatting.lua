6263b3530133d8e37f31679b5535dc8e689bd6db
Anyang Ateny <anyangateny@outlook.com>
2026-02-19 17:19:46 +1030

    feat: update nvim plugins

--- nvim/lua/custom/plugins/formatting.lua
+++ nvim/lua/custom/plugins/formatting.lua
@@ -6,6 +6,15 @@ return {
     {
       '<leader>f',
       function()
+        local bufname = vim.api.nvim_buf_get_name(0)
+        if vim.bo.filetype == 'cpp' or vim.bo.filetype == 'c' then
+          local style_file = vim.fs.find('.clang-format', { path = bufname, upward = true })[1]
+          if style_file then
+            vim.notify('clang-format: ' .. style_file, vim.log.levels.INFO)
+          else
+            vim.notify('clang-format: no .clang-format found, using default style', vim.log.levels.WARN)
+          end
+        end
         require('conform').format { async = true, lsp_format = 'fallback' }
       end,
       mode = '',
